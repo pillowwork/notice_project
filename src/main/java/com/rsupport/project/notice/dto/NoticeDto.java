bf48a14c7e74e741911f7fd3b9a7c5eeaf3df175
Pillow Ahn <pillow.work@gmail.com>
2025-02-04 14:45:00 +0900

    [develop] entity, repository - 공지사항의 entity, dto, repository 작성

--- src/main/java/com/rsupport/project/notice/dto/NoticeDto.java
+++ src/main/java/com/rsupport/project/notice/dto/NoticeDto.java
@@ -1,4 +1,49 @@
 package com.rsupport.project.notice.dto;
 
+import com.rsupport.project.notice.entity.NoticeEntity;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
 public class NoticeDto {
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Request {
+        @NotNull
+        private String title;
+        @NotNull
+        private String contents;
+        @NotNull
+        private LocalDateTime startDate;
+        @NotNull
+        private LocalDateTime endDate;
+        @NotNull
+        private String author;
+
+    }
+
+    @Getter
+    public static class Response {
+        private String title;
+        private String contents;
+        private LocalDateTime createDate;
+        private Integer viewCount;
+        private String author;
+
+        public Response(NoticeEntity entity) {
+            this.title = entity.getTitle();
+            this.contents = entity.getContents();
+            this.createDate = entity.getCreateDate();
+            this.viewCount = entity.getViewCount();
+            this.author = entity.getAuthor();
+        }
+
+    }
 }
--- src/main/java/com/rsupport/project/notice/entity/NoticeEntity.java
+++ src/main/java/com/rsupport/project/notice/entity/NoticeEntity.java
@@ -1,4 +1,37 @@
 package com.rsupport.project.notice.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notice")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class NoticeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String title;
+    @NotNull
+    @Column(columnDefinition = "TEXT")
+    private String contents;
+    @NotNull
+    private LocalDateTime startDate;
+    @NotNull
+    private LocalDateTime endDate;
+    @NotNull
+    private String author;
+
+    @Column(updatable = false)
+    private LocalDateTime createDate = LocalDateTime.now();
+    private int viewCount = 0;
+
 }
